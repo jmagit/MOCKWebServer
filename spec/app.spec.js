bfc78338ecb9b0bae4c0012f6f8bfd38a758c098
Javier <35864946+jmagit@users.noreply.github.com>
2022-07-15 10:29:31 +0200

    Jest

---
+++ spec/app.spec.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe("Test the root path", () => {
+    let spy
+    beforeAll(() => {
+        spy = jest.spyOn(console, 'log');
+        spy.mockImplementation(() => { })
+        return Promise.resolve()
+    });
+  it("It should response the GET method", done => {
+    request(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
