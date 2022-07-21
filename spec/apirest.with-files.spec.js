fdcf00a089451a8e9e33b69278d63dd3d1f15c83
Javier <jmartin@grupoloyal.com>
2022-07-22 01:46:44 +0200

    Limpieza de datos

--- spec/apirest.with-files.spec.js
+++ spec/apirest.with-files.spec.js
@@ -3,18 +3,24 @@ const request = require('supertest');
 const seguridad = require('../src/seguridad')
 const utils = require('../src/utils')
 
-const token = seguridad.generarTokenScheme({ "idUsuario": "admin", "password": "", "nombre": "Administrador", "roles": ["Usuarios", "Administradores"] })
+const usr = { "idUsuario": "admin", "password": "", "nombre": "Administrador", "roles": ["Usuarios", "Administradores", "Empleados"] }
+const token = seguridad.generarTokenScheme(usr)
+const cookie = `Authorization=${seguridad.generarTokenJWT(usr)};`
 
 describe('API Rest: Ficheros reales', () => {
     const app = require('../src/app');
     const serviciosConfig = utils.getServiciosConfig()
-    it.each(serviciosConfig.map(item => [item.endpoint.toUpperCase(), item.security]))('API %s', async (endpoint, security) => {
-        let auth = token;
-        if (typeof (security) === 'string')
-            auth = seguridad.generarTokenScheme({ "idUsuario": "admin", "password": "", "nombre": "Administrador", "roles": security.split(",") })
+    it.each(serviciosConfig.map(item => [item.endpoint.toUpperCase(), item.security]))('Autenticación bearer %s', async (endpoint) => {
         let response = await request(app)
             .get("/api/" + endpoint.toLowerCase())
-            .set('authorization', auth)
+            .set('authorization', token)
+        expect(response.statusCode).toBe(200)
+        expect(response.body.length).toBeGreaterThan(1)
+    });
+    it.each(serviciosConfig.map(item => [item.endpoint.toUpperCase(), item.security]))('Autenticación cookie %s', async (endpoint) => {
+        let response = await request(app)
+            .get("/api/" + endpoint.toLowerCase())
+            .set('Cookie', cookie)
         expect(response.statusCode).toBe(200)
         expect(response.body.length).toBeGreaterThan(1)
     });
